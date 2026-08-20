bbdb7b96f7a2fa192767a8c010110929daae0e97
abdofahd <abdofahd852@gmail.com>
2026-08-20 18:49:55 +0300

    Add health check endpoint returning JSON response

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+    ]);
+});
